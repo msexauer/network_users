03e7a2fffa0896227b2eeef66d7d733abae5aa12
Max Sexauer <msexauer@arbor.net>
2017-08-14 11:25:58 -0400

    With the exception of changes to be made for a specific network, this piece of code is functional and *finished*

--- network_scan.py
+++ network_scan.py
@@ -21,23 +21,23 @@ scan.scan(hosts='10.1.120.0/23', arguments='-sn') #Network space defined here
 
 #Main loop
 for host in scan.all_hosts():
-
-    #For debuging only
-    #print('{0} {1}'.format(host, scan[host].hostname()))
+    previous_hosts = []
+    current_host = [scan[host].hostname()]
 
     # Query database for data
     for row in c.execute('SELECT * FROM network'):
         query = list(row)
-        print()
-        print(query[2])
-        print(scan[host].hostname())
+        previous_hosts.append(query[2])
+
+    # If the host is not currently on the list, data_diff = True
+    if set(previous_hosts).intersection(current_host) == set([]):
+        data_diff = True
 
     #If data_diff, write differences to db
     if data_diff == True:
         with connection:
             c.execute('INSERT INTO network (date, ip, host) VALUES (?,?,?)', (today(), host, scan[host].hostname()))
-        data_diff = "false"
-        print("Write")
+        data_diff = False # Reset for next instance of loop
 
 #Wrap up stuff
 connection.commit
